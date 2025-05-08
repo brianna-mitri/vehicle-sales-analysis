faddfe280cd293369c2cbb97d898446a1c36212b
brianna-mitri <brianna.mitri@gmail.com>
2025-05-08 10:35:04 -0700

    Added raw data download

---
+++ ETL/01_download_data.py
@@ -0,0 +1,16 @@
+# imports
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pathlib
+
+# setup output path
+output_path = pathlib.Path('../Resources')
+output_path.mkdir(exist_ok=True, parents=True)
+
+# import dataset through kaggle api
+try:
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files('kyanyoga/sample-sales-data', path=output_path, unzip=True)
+    print(f'☑ Data downloaded. Local path: "{output_path}"')
+except Exception as e:
+    print(f'☐ Data NOT downloaded. Error occured: {e}')
