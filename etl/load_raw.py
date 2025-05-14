f471d99c8e120f1004f5ad890edb07071965ba79
brianna-mitri <brianna.mitri@gmail.com>
2025-05-14 13:45:39 -0700

    Defined new function for loading raw table only and started main driver function

--- etl/load_raw.py
+++ etl/load_raw.py
@@ -49,121 +49,201 @@ def build_buf(dict, col1='col1', col2='col2') -> io.StringIO:
     return buf
 
 # -------------------------------------
-# Load raw data into database
+# Functions: load raw data into database
 # -------------------------------------
-def main() -> None:
-    conn = cur = None
-    try:
-        # connect to postgresql server
-        conn = psycopg2.connect(target_dsn)
-        cur = conn.cursor()
-        print(f"☑ Connected to database")
-
-        # create temp staging table with no constraints
-        cur.execute(sql.SQL(
+# -------------------------- raw orders table --------------------------
+def load_raw_orders(cur) -> int:
+    '''
+    Load new records for raw orders csv table
+    '''
+    # create empty staging table 
+    cur.execute(sql.SQL(
             """
             CREATE TEMP TABLE stage AS
             SELECT * FROM {dest} LIMIT 0
             """
         ).format(dest=sql.Identifier(dest_table)))
-        print(f"☑ Staging table created")
+    
+    # copy data into staging
+    with open(raw_path, newline='', encoding='latin1') as fh:
+        # read each row as a dict
+        reader = csv.DictReader(fh)
+        # get header names
+        csv_cols = [c.lower() for c in reader.fieldnames]
 
-        # ---------------------------------
-        # load raw orders data
-        # ---------------------------------
-        with open(raw_path, newline='', encoding='latin1') as fh:
-            # read each row as a dict
-            reader = csv.DictReader(fh)
-            # get header names
-            csv_cols = [c.lower() for c in reader.fieldnames]
+        # put reordered rows in an in memory buffer ready for copy
+        buf = io.StringIO()
+        writer = csv.writer(buf, lineterminator='\n')
+        for row in reader:
+            writer.writerow([row.get(c.upper(), row.get(c)) for c in csv_cols])
+        buf.seek(0)
 
-            # put reordered rows in an in memory buffer ready for copy
-            buf = io.StringIO()
-            writer = csv.writer(buf, lineterminator='\n')
-            for row in reader:
-                writer.writerow([row.get(c.upper(), row.get(c)) for c in csv_cols])
-            buf.seek(0)
+        # copy into staging
+        copy_sql = sql.SQL(
+            "COPY stage ({cols}) FROM STDIN WITH (FORMAT CSV)"
+        )
+        cur.copy_expert(
+            copy_sql.format(cols=sql.SQL(',').join(map(sql.Identifier, csv_cols))),
+            buf
+        )
+    # insert from staging into actual destination table
+    insert_sql = sql.SQL(
+        """
+        INSERT INTO {dest} ({cols})
+        SELECT {cols} FROM stage
+        ON CONFLICT (ordernumber, orderlinenumber) DO NOTHING
+        """
+    ).format(
+        dest = sql.Identifier(dest_table),
+        cols = sql.SQL(',').join(map(sql.Identifier, csv_cols))
+    )
+    cur.execute(insert_sql)
+    
+    # return new raw records count
+    return cur.rowcount
+    
 
-            # copy into staging
-            copy_sql = sql.SQL(
-                "COPY stage ({cols}) FROM STDIN WITH (FORMAT CSV)"
-            )
-            cur.copy_expert(
-                copy_sql.format(cols=sql.SQL(',').join(map(sql.Identifier, csv_cols))),
-                buf
-            )
-            print(f"☑ Data copied into staging")
-
-            # insert from staging into actual destination table
-            insert_sql = sql.SQL(
-                """
-                INSERT INTO {dest} ({cols})
-                SELECT {cols} FROM stage
-                ON CONFLICT (ordernumber, orderlinenumber) DO NOTHING
-                """
-            ).format(
-                dest = sql.Identifier(dest_table),
-                cols = sql.SQL(',').join(map(sql.Identifier, csv_cols))
-            )
-            cur.execute(insert_sql)
-            new_records = cur.rowcount
-
-            print(f"☑ {new_records} new records loaded to {target_db}'s table: {dest_table}")
-
-        # ---------------------------------
-        # first load: load country codes
-        # ---------------------------------
-        # check if table is empty
-        cur.execute("SELECT 1 FROM iso_country_codes LIMIT 1;")
-        already_loaded = cur.fetchone() is not None
-
-        # if table not filled already then load
-        if not already_loaded:
-            # ------------- Load ISO country codes -------------
-            # create in memory csv of iso codes
-            iso_codes_dict = {country.alpha_3: country.name for country in pycountry.countries}
-            iso_codes_buf = build_buf(iso_codes_dict, col1='alpha3', col2='name')
-
-            # load
-            cur.copy_expert(
-                '''
-                COPY iso_country_codes(alpha3, name) 
-                FROM STDIN CSV HEADER
-                ''',
-                iso_codes_buf
-            )
-            print(f'☑ ISO country codes table filled')
-
-            # ------------- Load ISO country aliases -------------
-            alias_buf = build_buf(aliases, col1='alias', col2='alpha3')
-            cur.copy_expert(
-                '''
-                COPY iso_country_aliases(alias,alpha3)
-                FROM STDIN CSV HEADER
-                ''',
-                alias_buf
-            )
-            print(f'☑ ISO country aliases table filled')
-        else:
-            print(f'☐ ISO country codes already present--> skipping...')
         
-        # commit changes and display
-        conn.commit()
+        
+
+# def main_old() -> None:
+#     conn = cur = None
+#     try:
+#         # connect to postgresql server
+#         with psycopg2.connect(target_dsn) as conn:
+#             with conn.cursor() as cur:
+#                 print(f'☑ Connected to database')
+
+#                 new_records = load_raw_orders(cur)
+
+#         cur = conn.cursor()
+#         print(f"☑ Connected to database")
+
+#         # create temp staging table with no constraints
+#         cur.execute(sql.SQL(
+#             """
+#             CREATE TEMP TABLE stage AS
+#             SELECT * FROM {dest} LIMIT 0
+#             """
+#         ).format(dest=sql.Identifier(dest_table)))
+#         print(f"☑ Staging table created")
+
+#         # ---------------------------------
+#         # load raw orders data
+#         # ---------------------------------
+#         with open(raw_path, newline='', encoding='latin1') as fh:
+#             # read each row as a dict
+#             reader = csv.DictReader(fh)
+#             # get header names
+#             csv_cols = [c.lower() for c in reader.fieldnames]
+
+#             # put reordered rows in an in memory buffer ready for copy
+#             buf = io.StringIO()
+#             writer = csv.writer(buf, lineterminator='\n')
+#             for row in reader:
+#                 writer.writerow([row.get(c.upper(), row.get(c)) for c in csv_cols])
+#             buf.seek(0)
+
+#             # copy into staging
+#             copy_sql = sql.SQL(
+#                 "COPY stage ({cols}) FROM STDIN WITH (FORMAT CSV)"
+#             )
+#             cur.copy_expert(
+#                 copy_sql.format(cols=sql.SQL(',').join(map(sql.Identifier, csv_cols))),
+#                 buf
+#             )
+#             print(f"☑ Data copied into staging")
+
+#             # insert from staging into actual destination table
+#             insert_sql = sql.SQL(
+#                 """
+#                 INSERT INTO {dest} ({cols})
+#                 SELECT {cols} FROM stage
+#                 ON CONFLICT (ordernumber, orderlinenumber) DO NOTHING
+#                 """
+#             ).format(
+#                 dest = sql.Identifier(dest_table),
+#                 cols = sql.SQL(',').join(map(sql.Identifier, csv_cols))
+#             )
+#             cur.execute(insert_sql)
+#             new_records = cur.rowcount
+
+#             print(f"☑ {new_records} new records loaded to {target_db}'s table: {dest_table}")
+
+#         # ---------------------------------
+#         # first load: load country codes
+#         # ---------------------------------
+#         # check if table is empty
+#         cur.execute("SELECT 1 FROM iso_country_codes LIMIT 1;")
+#         already_loaded = cur.fetchone() is not None
+
+#         # if table not filled already then load
+#         if not already_loaded:
+#             # ------------- Load ISO country codes -------------
+#             # create in memory csv of iso codes
+#             iso_codes_dict = {country.alpha_3: country.name for country in pycountry.countries}
+#             iso_codes_buf = build_buf(iso_codes_dict, col1='alpha3', col2='name')
+
+#             # load
+#             cur.copy_expert(
+#                 '''
+#                 COPY iso_country_codes(alpha3, name) 
+#                 FROM STDIN CSV HEADER
+#                 ''',
+#                 iso_codes_buf
+#             )
+#             print(f'☑ ISO country codes table filled')
+
+#             # ------------- Load ISO country aliases -------------
+#             alias_buf = build_buf(aliases, col1='alias', col2='alpha3')
+#             cur.copy_expert(
+#                 '''
+#                 COPY iso_country_aliases(alias,alpha3)
+#                 FROM STDIN CSV HEADER
+#                 ''',
+#                 alias_buf
+#             )
+#             print(f'☑ ISO country aliases table filled')
+#         else:
+#             print(f'☐ ISO country codes already present--> skipping...')
+        
+#         # commit changes and display
+#         conn.commit()
 
 
-    except (psycopg2.Error, OSError, csv.Error) as e:
-        if conn:
-            conn.rollback()
-            print(f"☐ Data NOT loaded due to error: {e}")
-            raise e
-    finally:
-        # close network
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
+#     except (psycopg2.Error, OSError, csv.Error) as e:
+#         if conn:
+#             conn.rollback()
+#             print(f"☐ Data NOT loaded due to error: {e}")
+#             raise e
+#     finally:
+#         # close network
+#         if cur:
+#             cur.close()
+#         if conn:
+#             conn.close()
+#         print(f"☑ Database, {target_db}, connection closed")
+        
+# -------------------------- main driver --------------------------
+def main():
+    try:
+        # connect to postgresql server
+        with psycopg2.connect(target_dsn) as conn:
+            with conn.cursor() as cur:
+                print(f"☑ Connected to database")
+
+                # load raw table
+                new_records = load_raw_orders(cur)
+                print(f"☑ {new_records} new records loaded to {target_db}'s table: {dest_table}")
+            
+            conn.commit()
+            print(f"☑ Commited changes to database: {target_db}")
+
         print(f"☑ Database, {target_db}, connection closed")
-        
+
+    except Exception as e:
+        print(f"☐ Data NOT loaded due to error: {e}")
+        raise
 
 if __name__ == '__main__':
     main()
