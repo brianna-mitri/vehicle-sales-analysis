e44e97b9aaa149f8ac804fa87c4e05df60776a8a
brianna-mitri <brianna.mitri@gmail.com>
2025-05-29 10:24:11 -0700

    Functioning updating phone numbers and checking validation in db

--- etl/transform_phone.py
+++ etl/transform_phone.py
@@ -30,17 +30,24 @@ def update_phone(raw_phone, country):
     '''
     takes phone number and alpha2 code and returns valid phone or none
     '''
-    # try to parse number
+    phone = raw_phone
+    is_valid = False
+
+    # try to validate
     try:
         parsed_phone = phonenumbers.parse(raw_phone, country)
+
+        if is_valid_number(parsed_phone):
+            phone = format_number(parsed_phone, PhoneNumberFormat.INTERNATIONAL)
+            is_valid = True
     except NumberParseException:
-        return None
+        print('passing...')
+        pass
     
-    # return reformatted number if valid
-    if is_valid_number(parsed_phone):
-        return format_number(parsed_phone, PhoneNumberFormat.INTERNATIONAL)
-    else:
-        return None
+    return {
+        'phone': phone,
+        'phone_valid': is_valid
+    }
 
 
 #################################################
@@ -52,7 +59,7 @@ with psycopg2.connect(dsn) as conn, conn.cursor() as cur:
     # get last customer id 
     cur.execute(
         '''
-        SElECT last_id FROM etl_watermark
+        SELECT last_id FROM etl_watermark
         WHERE target = 'phone_val'
         '''
     )
@@ -61,10 +68,16 @@ with psycopg2.connect(dsn) as conn, conn.cursor() as cur:
     # get newest records
     cur.execute(
     '''
-    SELECT  customer_id, phone, phone_valid
-    FROM customers
-    WHERE customer_id > %s
-        AND phone_valid is NULL
+    SELECT	c.customer_id,
+		    c.phone,
+		    c.phone_valid,
+		    ic.alpha2
+    FROM customers c
+    JOIN addresses a ON c.customer_id = a.customer_id
+    JOIN iso_country_codes ic ON a.country_code = ic.alpha3
+    WHERE c.customer_id > %s
+        AND c.phone_valid IS NULL
+    ORDER BY c.customer_id
     ''', (last_id,))
     
     records = cur.fetchall()
@@ -85,6 +98,49 @@ with psycopg2.connect(dsn) as conn, conn.cursor() as cur:
     max_seen = last_id
 
     # -------------------------- update phone numbers --------------------------
+    for r in records:
+        # set variables
+        phone_info = dict(zip(fields, r))
+        max_seen = max(max_seen,phone_info['customer_id'])
+        raw_phone = phone_info['phone']
+        phone_valid = phone_info['phone_valid']
+        country = phone_info['alpha2']
+        
+        # validate phone
+        print(f'\tChecking customer_id {phone_info['customer_id']}')
 
+        parsed_dict = update_phone(raw_phone, country)
+        phone = parsed_dict['phone']
+        phone_valid = parsed_dict['phone_valid']
 
-print(update_phone('2125557818', 'US'))
+        try:
+            # update phones
+            cur.execute(
+                '''
+                UPDATE customers
+                    SET phone       = %s,
+                        phone_valid = %s
+                WHERE customer_id = %s
+                    AND phone_valid IS NULL
+                ''', (
+                    phone, phone_valid, phone_info['customer_id']
+                )
+            )
+        except Exception as e:
+            print(f'\n⚠ ERROR: {e}')
+        else:
+            conn.commit()
+
+    # -------------------------- advance watermark --------------------------
+    if max_seen > last_id:
+        cur.execute(
+            '''
+            UPDATE etl_watermark
+                SET last_id = %s,
+                    updated_at = now()
+                WHERE target = 'phone_val'
+            ''', (max_seen,)
+        )
+        conn.commit()
+
+print('\n☑ Updating/validating phone numbers done')
